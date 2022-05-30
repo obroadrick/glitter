36160b20c3d3750499f38277a71466961c1bf1bd
Oliver Broadrick <odbroadrick@gmail.com>
2022-05-30 15:23:23 -0400

    vc

--- glitter_characterization_big.m
+++ glitter_characterization_big.m
@@ -2,9 +2,13 @@
 % since we can't fit all the images into memory at once
 % we just maintain a 'max' image
 ims = [];
+idxs = [];
 m = [];
 i = 0;
 colormap(gray);
+t = tiledlayout(2,1);
+t.Padding = 'compact';
+t.TileSpacing = 'compact';
 for ix = 0:3:765
     i = i + 1;
     p = '/Users/oliverbroadrick/Desktop/glitter-stuff/Calibrations5-25/';
@@ -25,11 +29,22 @@ for ix = 0:3:765
     if i == 1
         % for the first image, make it the current max
         ims(:,:,2) = im;
+        idxs = im;
+        idxs(:,:) = 1;
     end
-    ims(:,:,2) = max(ims,[],3);
-    imagesc(ims(:,:,2));
-    title(i);
+    % max image, fake indexes 
+    [ims(:,:,2), tempidxs] = max(ims,[],3);
+    % real indexes (1 -> from new image, 2 -> leave as before)
+    idxs(tempidxs == 1) = i;
+
+    % show
+    ax1 = nexttile(1); 
+    imagesc(ims(:,:,2));title(i);
     drawnow;
+    ax2 = nexttile(2);
+    imagesc(idxs);title('indexes');
+    drawnow;
+    
 end
 %% show the max image of glitter
 imagesc(ims(:,:,2));colormap(gray);
@@ -44,8 +59,8 @@ m = ims(:,:,2);
 %todo (aruco?)
 
 %% Find specs of glitter using max image of all images
-m = max(ims,[],3);
-imagesc(m);colormap(gray);
+%m = max(ims,[],3);
+%imagesc(m);colormap(gray);
 
 %% filter image to keep only specs of glitter bright enough to be of interest
 F = fspecial('Gaussian',[15 15],1) - fspecial('Gaussian',[15 15],7);
@@ -56,18 +71,49 @@ imagesc(M);colormap(gray);
 thresh = 30;
 imagesc(M > thresh);colormap(gray);
 
-%% get a list of the points
+%% get a list of the spec centroids
+overlap_threshold = 8; % diff in index >= thresh are two specs maybe
 numPoints = 0;
 C = [];
+D = [];
 Mt = M>thresh;
-imagesc(Mt);
-R = regionprops(Mt);
+%imagesc(Mt);
+R = regionprops(Mt,'Centroid','PixelIdxList');
+numBad = 0;
 for rx = 1:size(R,1)
+    r = max(idxs(R(rx).PixelIdxList)) - min(idxs(R(rx).PixelIdxList));
     P = R(rx).Centroid;
     numPoints = numPoints + 1;
+    if r > overlap_threshold
+        numBad = numBad + 1;
+        D(numBad,:) = P;
+    end
     C(numPoints,:) = P;
 end
+%% draw the detected bad centroids on top of the glitter specs max image
+imagesc(m);colormap(gray);
+for cx = 1:size(D,1)
+    axis on;
+    hold on;
+    plot(D(cx,1),D(cx,2), 'r+', 'MarkerSize', 12, 'LineWidth', 2);
+end
+%% salvage these 'bad' regions by finding 2 centroids instead
+newCentroids = []
+for rx = 1:size(R,1)
+    r = max(idxs(R(rx).PixelIdxList)) - min(idxs(R(rx).PixelIdxList));
+    P = R(rx).Centroid;
+    numPoints = numPoints + 1;
+    if r < overlap_threshold
+        % ignore 'good' regions
+        continue
+    end
+    numBad = numBad + 1;
 
+    % add the two new centroids
+    newCentroids(2*numBad-1,:) = P;
+    newCentroids(2*numBad,:) = P;
+    
+end
 %% draw the centroids on top of the glitter specs max image
 imagesc(m);
 for cx = 15000:17000%size(C,1)
@@ -79,6 +125,7 @@ end
 %% brightness distributions for some random specs' centroids
 % (for the various lighting positions)
 num = 16;
+rng(3141592);
 cxs = randi(size(C,1),num,1);
 numims = 256;
 dists = zeros(num,numims);
@@ -127,6 +174,7 @@ end
 %% let's look at the specific sampled specs' centroids now
 % draw the centroids on top of the glitter specs max image
 imagesc(m);colormap(gray);
+overlap_threshold = 10;
 for ix=1:size(cxs,1)
     cx = cxs(ix);
     hold on;
@@ -134,3 +182,48 @@ for ix=1:size(cxs,1)
     text(C(cx,1),C(cx,2), cellstr(num2str(ix)), 'FontSize', 12, 'Color', [1 0 0]);
 end
 
+%% make a tethered set of plots:
+colormap(gray);
+t = tiledlayout(2,2);
+t.Padding = 'compact';
+t.TileSpacing = 'compact';
+% max image
+ax1 = nexttile(1); 
+imagesc(ims(:,:,2));title('max image');
+% indexes
+ax2 = nexttile(2);
+imagesc(idxs);title('indexes');
+% filtered and thresholded binary image
+ax3 = nexttile(3);
+imagesc(M > thresh);title('binary image after filter/threshold');
+% found centroids
+ax4 = nexttile(4);
+imagesc(m);
+for cx = 15000:17000%size(C,1)
+    axis on;
+    hold on;
+    plot(C(cx,1),C(cx,2), 'r+', 'MarkerSize', 12, 'LineWidth', 2);
+end
+title('spec locations found (region centroids after max, filter, threshold)');
+% link axes
+linkaxes([ax1 ax2 ax3 ax4]);
+
+%% find regions with indexes over a high range (overlapping specs maybe)
+numPoints = 0;
+C = [];
+Mt = M>thresh;
+imagesc(Mt);
+R = regionprops(Mt,'Centroid','PixelIdxList');
+ranges = zeros(size(R,1),1);
+for rx = 1:size(R,1)
+    ranges(rx) = max(idxs(R(rx).PixelIdxList)) - min(idxs(R(rx).PixelIdxList));
+    P = R(rx).Centroid;
+    numPoints = numPoints + 1;
+    C(numPoints,:) = P;
+end
+histogram(ranges);title('Histogram of Index Variation within Regions');
+%% so, we use a threshold to detect overlapping specs
+overlap_threshold = 10;
+
+%%
+disp('helloworld');
