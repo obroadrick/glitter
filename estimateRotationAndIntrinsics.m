fa4636decb2a922fa260faa12b5f06ddb845513f
Oliver Broadrick <odbroadrick@gmail.com>
2022-06-21 09:57:33 -0400

    ransac translation estimates

---
+++ estimateRotationAndIntrinsics.m
@@ -0,0 +1,3 @@
+% estimate rotation and focal length
+% this is a very simple version of camera calibration
+% 
--- estimateTranslation.m
+++ estimateTranslation.m
@@ -27,24 +27,30 @@ canonicalCentroids = [out(:,1) out(:,2) zeros(size(out,1),1)];
 %% match canonical centroids to those in the characterization
 knownCanonicalCentroids = matfile(P.canonicalCentroids).canonicalCentroids;
 [idx, dist] = knnsearch(knownCanonicalCentroids, canonicalCentroids, 'Distance', 'euclidean');
-% only consider specs whose match is within .5 millimeters
-closeEnough = .1;
+% only consider specs whose match is within .xx millimeters
+closeEnough = .3;
 specIdxs = idx(dist<closeEnough);
 specPos = knownCanonicalCentroids(specIdxs,:);
 allSpecNormals = matfile(P.specNormals).specNormals;
 specNormals = allSpecNormals(specIdxs,:);
 %% also show the original max image so that we can compare the centroids
 % that we found with the centroids that we characterized from the start
-tiledlayout(2,1);colormap(gray);
+tiledlayout(1,1);colormap(gray);
 ax3 = nexttile;
-imagesc(im);
+imagesc(im); hold on;
+% now show the canonical spec centroids (mapped onto this image coordinate
+% system using the inverse homography) to show them 
+tforminv = invert(tform);
+characterizedCentroidsOnThisImage = transformPointsForward(tforminv, [knownCanonicalCentroids(:,1) knownCanonicalCentroids(:,2)]);
+plot(characterizedCentroidsOnThisImage(:,1),characterizedCentroidsOnThisImage(:,2),'r+');
+plot(imageCentroids(:,1),imageCentroids(:,2),'bx');
 maxImagePath = '/Users/oliverbroadrick/Desktop/glitter-stuff/glitter-repo/data/maxImage.jpg';
 maxImage = imread(maxImagePath);
 knownImageCentroids = matfile(P.imageCentroids).imageCentroids;
-ax4 = nexttile;
-imagesc(im); hold on;
-plot(knownImageCentroids(:,1),knownImageCentroids(:,2),'r+');
-linkaxes([ax3 ax4]);
+%ax4 = nexttile;
+%imagesc(maxImage); hold on;
+%plot(knownImageCentroids(:,1),knownImageCentroids(:,2),'r+');
+%linkaxes([ax3 ax4]);
 
 %% reflect rays from light off specs
 % compute reflected rays: Ri = Li − 2(Li dot Ni)Ni
@@ -89,19 +95,25 @@ mx = [-M.GLIT_TO_MON_EDGES_X -M.GLIT_TO_MON_EDGES_X+M.MON_WIDTH_MM -M.GLIT_TO_MO
 my = [-M.GLIT_TO_MON_EDGES_Y+M.MON_HEIGHT_MM -M.GLIT_TO_MON_EDGES_Y+M.MON_HEIGHT_MM -M.GLIT_TO_MON_EDGES_Y -M.GLIT_TO_MON_EDGES_Y]; 
 mz = [M.GLIT_TO_MON_PLANES M.GLIT_TO_MON_PLANES M.GLIT_TO_MON_PLANES M.GLIT_TO_MON_PLANES]; 
 mc = ['g'];
-legendItems(size(legendItems,1)+1) = patch(mx,my,mz,mc,'DisplayName','Monitor');
+legendItems(size(legendItems,2)+1) = patch(mx,my,mz,mc,'DisplayName','Monitor');
 % table: (made up coords, doesn't matter, mostly for fun)
 tx = [-400 -400 600 600];
 ty = [-120 -120 -120 -120];
 tz = [-250 1000 1000 -250];
 tc = ['k'];
-legendItems(size(legendItems,1)+1) = patch(tx,ty,tz,tc,'DisplayName','Table');
+legendItems(size(legendItems,2)+1) = patch(tx,ty,tz,tc,'DisplayName','Table');
 % show camera as a dot: (dots=cameras)
 cam=camPosEst;
-legendItems(size(legendItems,1)+1) = scatter3(cam(1),cam(2),cam(3),'filled','red','DisplayName','Camera');
+legendItems(size(legendItems,2)+1) = scatter3(cam(1),cam(2),cam(3),100,'red','o','filled','DisplayName','Estimated Camera');
 disp(cam);
+% shown known ground truth camera position
+knownCamPos = matfile(P.camPos).camera_in_glitter_coords;
+disp(knownCamPos);
+disp('difference (error):');
+disp(norm(cam - knownCamPos));
+legendItems(size(legendItems,2)+1) = scatter3(knownCamPos(1),knownCamPos(2),knownCamPos(3),100,'blue','filled','o','DisplayName','True Camera');
 % show light source as a dot
-legendItems(size(legendItems,1)+1) = scatter3(lightPos(1),lightPos(2),lightPos(3),'filled','DisplayName','Light');
+legendItems(size(legendItems,2)+1) = scatter3(lightPos(1),lightPos(2),lightPos(3),'filled','DisplayName','Light');
 % draw all the passed lines
 %scale for drawing
 specNormals = specNormals .* 50;
@@ -114,12 +126,20 @@ for ix=1:size(R,1)
     z = [specNormals(ix,3)+specPos(ix,3) specPos(ix,3)]';
     line(x,y,z);
 end
+% get normalized spec distances for drawing
+distNormalized = dist / closeEnough;% color code for
 for ix=1:size(R,1)
     % draw reflected rays
     x = [specPos(ix,1) specPos(ix,1)+Rtocam(ix,1)]';
     y = [specPos(ix,2) specPos(ix,2)+Rtocam(ix,2)]';
     z = [specPos(ix,3) specPos(ix,3)+Rtocam(ix,3)]';
-    line(x,y,z);
+    %distance form camera estimate
+    green = [0 1 0];
+    red = [1 0 0];
+    c = min(1, distNormalized(ix));
+    color = green + c*(red-green);
+    %color = [1 c 1];
+    line(x,y,z,'Color',color);
 end
 % set viewpoint:
 view([-110 -30]);
